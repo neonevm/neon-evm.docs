f203e631324ed8b5b01b7793d36a23f8090c6f09
Bejoy <casperyourweb@gmail.com>
2021-08-24 16:41:34 +0300

    new url list

---
+++ src/components/WelcomeFeatures/index.js
@@ -0,0 +1,79 @@
+import React from 'react';
+import clsx from 'clsx';
+import styles from './WelcomeFeatures.module.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import Link from '@docusaurus/Link';
+import { faNewspaper, faCompass, faListAlt } from '@fortawesome/free-regular-svg-icons'
+
+
+const FeatureList = [
+  {
+    title: 'Getting Started',
+    icon: faCompass,
+    url: '/docs/getting_started',
+    description: (
+      <>
+        Fast discover Neon EVM. Community and Wiki sources.
+      </>
+    ),
+  },
+  {
+    title: 'Introduction',
+    icon: faNewspaper,
+    url: '/docs/intro',
+    description: (
+      <>
+        Short technology presentation, writing in couple paragraphs
+      </>
+    ),
+  },
+  {
+    title: 'Glossary',
+    icon: faListAlt,
+    url: '/docs/glossary',
+    description: (
+      <>
+        List of definitions, which used in the documentation.
+      </>
+    ),
+  },
+];
+
+function Feature({title, icon, url, description}) {
+  return (
+    <div className={clsx('col col--4')} className={styles.feature}>
+      <Link to={url} className={styles.featureCard}>
+        <div className={styles.featureCardHeader}>
+          <FontAwesomeIcon icon={icon}  size='lg'/>
+          <h3 className={styles.featureCardTitle}>{title}</h3>
+        </div>
+        <p>{description}</p>
+      </Link>
+    </div>
+  );
+}
+
+export default function WelcomeFeatures() {
+ return (
+   <section className={styles.features}>
+     <div className="container">
+       <div className="row">
+         {FeatureList.map((props, idx) => (
+           <Feature key={idx} {...props} />
+         ))}
+       </div>
+     </div>
+   </section>
+ );
+}
+
+// export default function HomepageFeatures() {
+//   return (
+//     <section className={styles.features}>
+//       <div className="container">
+//         <div className="row">
+//         </div>
+//       </div>
+//     </section>
+//   );
+// }
