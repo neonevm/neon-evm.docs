c234efe338e50ec5adea9748a62fa987e109e8de
Bejoy <casperyourweb@gmail.com>
2021-08-26 10:16:14 +0300

    fix meta

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ export default function Home() {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="Learn what Neon EVM is and start building on Neon now">
         <WelcomeFeatures />
     </Layout>
   );
