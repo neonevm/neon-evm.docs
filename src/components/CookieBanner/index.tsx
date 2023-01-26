11d920b1cf032eba4e139da7d0dec5ec47599fcc
Igor Prokopev <ip@neonlabs.org>
2023-01-26 13:49:09 +0700

    Refactoring cookie policy component.

--- src/components/CookieBanner/index.tsx
+++ src/components/CookieBanner/index.tsx
@@ -1,63 +1,53 @@
-import React, { useEffect, useState } from 'react';
-import { useThemeConfig } from '@docusaurus/theme-common';
+import React, { useEffect, useMemo, useState } from 'react';
 import './styles.css';
 
+const COOKIES_EXPIRE = 'cookies:expire';
+const COOKIES_ACCEPTED = 'cookies:accepted';
+
 export const CookieBanner = () => {
-  const { cookieBanner } = useThemeConfig();
-  const [ accepted, setAccepted ] = useState(false);
+  const [show, setShow] = useState(false);
 
-  const isPostponeExpired = () => {
-    const expire = localStorage.getItem('cookies:expire');
-    if (!expire) {
-      return false;
+  const isPostponeExpired = useMemo<boolean>(() => {
+    const expire = localStorage.getItem(COOKIES_EXPIRE);
+    if (typeof expire === 'string') {
+      return Math.abs(Number(expire) - new Date().getTime()) / 36e5 > 24;
     }
-    return Math.abs(Number(expire) - new Date().getTime()) / (60 * 60 * 1000) > 24;
-  }
+    return true;
+  }, []);
 
   useEffect(() => {
-    if (isPostponeExpired()) {
-      setAccepted(false)
-    } else {
-      const storageAccepted = localStorage.getItem('cookies:accepted') === 'true'
-
-      setAccepted(storageAccepted)
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem('cookies:accepted', accepted ? 'true' : 'false')
-  }, [accepted])
+    setShow(isPostponeExpired && !JSON.parse(localStorage.getItem(COOKIES_ACCEPTED)));
+  }, [isPostponeExpired]);
 
   const acceptCookies = () => {
-    setAccepted(true)
-
-    localStorage.removeItem('cookies:expire')
-  }
+    setShow(false);
+    localStorage.setItem(COOKIES_ACCEPTED, 'true');
+    localStorage.removeItem(COOKIES_EXPIRE);
+  };
 
   const postponeCookies = () => {
-    localStorage.setItem('cookies:expire', new Date().getTime().toString())
+    setShow(false);
+    localStorage.setItem(COOKIES_ACCEPTED, 'false');
+    localStorage.setItem(COOKIES_EXPIRE, new Date().getTime().toString());
+  };
 
-    setAccepted(true)
-  }
-
-  if (!cookieBanner || accepted) {
-    return null
-  }
-
-  return <>
-    <div className='cookie-banner'>
-      <p className='title'>
-        We use cookies
-      </p>
-      <p className='description'>
-        If you continue browsing, we consider that you have accepted <a href='https://neon-labs.org/cookie-policy' target='_blank'>cookies policy</a>.
-      </p>
-      <div className='actions'>
-        <button className='main' onClick={acceptCookies}>Accept</button>
-        <button className='secondary' onClick={postponeCookies}>Ask me later</button>
+  if (show) {
+    return <>
+      <div className='cookie-banner'>
+        <h4 className='title'>We use cookies</h4>
+        <p className='description'>
+          If you continue browsing, we consider that you have show <a
+          href='https://neon-labs.org/cookie-policy' target='_blank'>cookies policy</a>.
+        </p>
+        <div className='actions'>
+          <button className='main' onClick={acceptCookies}>Accept</button>
+          <button className='secondary' onClick={postponeCookies}>Ask me later</button>
+        </div>
       </div>
-    </div>
-  </>
-}
+    </>;
+  }
 
-export default CookieBanner
+  return null;
+};
+
+export default CookieBanner;
