68459535517ad6284df94156cf2604aa523dfd51
Igor Prokopev <ip@neonlabs.org>
2023-05-19 18:54:49 +0700

    Change algolia config.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -33,12 +33,12 @@ const config = {
       image: 'img/og_image.png',
       cookieBanner: {},
       algolia: {
-        apiKey: 'f091c9673db5f9d6b08e940901f8ede5',
-        appId: '879GT1L978',
-        indexName: 'neonfoundation',
+        apiKey: 'b42bf0be9b7f964aa534f802164b53f8',
+        appId: 'IMU5IHYKIJ',
+        indexName: 'neon-labs',
         contextualSearch: true,
         placeholder: 'search something...',
-        algoliaOptions: { facetFilters: ['language:en'] },
+        algoliaOptions: { facetFilters: ['type:$TYPE'] },
         debug: false,
         dropdown: true
       },
