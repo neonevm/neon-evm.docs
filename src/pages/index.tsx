0a9d1960b9a5f076c184f56c407c6619a10f90cb
Evgeniya <nemezidaxxx@mail.ru>
2023-07-25 15:47:39 +0200

    Google analytics implementation (#283)
    * recreates edits for fresh branch
    
    * recreates edits for fresh branch
    
    * recreates edits for fresh branch
    
    * recreates edits for fresh branch
    
    * recreates edits for fresh branch
    
    * fixes missing image from PR recreate
    
    * updates docs with slack comments
    
    * fix: ? to menu
    
    * starts: gasless
    
    * teaks before requesting further feedback
    
    * first draft: for discussion
    
    * adds boilerplate
    
    * small update
    
    * spurious edit to reopen PR
    
    * supports boilerplate code without exposing team member's details
    
    * supports heap overflow troubleshooting
    
    * QA round updates
    
    * adds sample code
    
    * adds contact point for requesting gasless pack
    
    * chasing this through for PR comment
    
    * adds notes
    
    * supports local proxy with docker
    
    * doc page for review
    
    * what about history?
    
    * fixes: 404
    
    * fixes broken links
    
    * Enable broken links check
    
    * Add external links check
    
    * Fix ledger link
    
    * Switch to markdown-link-check
    
    * fixes: links and ids false positives
    
    * prod-161: updates links to satisfy linter
    
    * Ignore false positive links
    
    * attempts html comment in place of yaml, makes private repo link redundant
    
    * Add distinct pattern domains for false positive links
    
    * adds successful approach: html comment in metadata
    
    * adds successful approach: html comment in metadata
    
    * Add example snippet for the gas price
    
    * Add mdx to link checker
    
    * applies test snippet as real
    
    * fix for link affecting build
    
    * retires beta and zero gas fees
    
    * retires Neon testnet
    
    * wip: starts opcode update
    
    * adds: new unsupported opcode
    
    * Add information about vesting-contract-cli
    
    * Add audits
    
    * feat: add google analytics
    
    ---------
    
    Co-authored-by: m4sterbunny <m4sterbunny@gmail.com>
    Co-authored-by: Juliano Souza <46322485+thespamer@users.noreply.github.com>
    Co-authored-by: Arkady Irincheev <83811228+arkadyone@users.noreply.github.com>
    Co-authored-by: Aleksandr Liubenkov <lubenkov.a@gmail.com>
    Co-authored-by: Semen Medvedev <sm@neonlabs.org>
    Co-authored-by: Andrey Falaleev <andrew.falaleev@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { Redirect } from '@docusaurus/router';
+import ReactGA from "react-ga4";
+
+ReactGA.initialize("G-S3B89Y6LRP");
 
 export default function Home(): JSX.Element {
   return <Redirect to='/docs/quick_start' />;
