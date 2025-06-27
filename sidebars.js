63279a7364bc8a5c55c7f62ee209e6b9ac5cac87
OlehSyzonov <137310889+OlehSyzonov@users.noreply.github.com>
2025-06-27 19:00:21 +0200

    * debug

--- sidebars.js
+++ sidebars.js
@@ -3,7 +3,7 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-    { type: 'html', value: '<h4 class="sidebar-menu-title">"Introduction"</h4>' },
+    { type: 'html', value: '<h4 class="sidebar-menu-title">Introduction</h4>' },
     {
       type: 'doc',
       label: 'Quick Start',
