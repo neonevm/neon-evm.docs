4064b9932b362b3a2c3427a803b09bba28363b7d
OlehSyzonov <137310889+OlehSyzonov@users.noreply.github.com>
2025-06-27 10:24:33 +0200

    Update sidebars.js
    * Restructure

--- sidebars.js
+++ sidebars.js
@@ -3,45 +3,98 @@
 /** @type {import('@docusaurus/plugin-content-docs').SidebarsConfig} */
 const sidebars = {
   tutorialSidebar: [
-    { type: 'html', value: '<h4 class="sidebar-menu-title">Get started</h4>' },
+    { type: 'html', value: '<h4 class="sidebar-menu-title"Introduction</h4>' },
     {
       type: 'doc',
-      label: '🏓 Quick Start',
+      label: 'Quick Start',
       id: 'quick_start'
     },
     {
       type: 'category',
-      label: '🧬 Neon EVM Overview',
+      label: 'Demos',
+      items: ['about/Memecoin_Launchpad_Raydium_Integration', 'about/POC_Aave_V3_flash_loan_with_composability_requests_to_Solana']
+    },
+    {
+      type: 'category',
+      label: 'Token',
+      items: ['tokens/neon_token', 'tokens/gas_fees', 'tokens/token_list', 'token_transferring/neonpass_usage', 'token_transferring/neon_transfer']
+    },
+    { type: 'html', value: '<div class="sidebar-menu-divider"/>' },
+    { type: 'html', value: '<h4 class="sidebar-menu-title">Building Solana Native EVM dApps</h4>' },
+    {
+      type: 'doc',
+      label: 'Common Solana Terminology',
+      id: 'composability/common_solana_terminology'
+    },
+    {
+      type: 'doc',
+      label: 'ICallSolana Interface',
+      id: 'composability/call_solana_interface'
+    },
+    {
+      type: 'category',
+      label: 'Composability Libraries',
+      items: [
+        'composability/composability_libraries',
+        'composability/system_program_composability_libraries',
+        'composability/spl_token_program_composability_libraries',
+        'composability/metaplex_program_composability_libraries',
+        'composability/associated_token_program_composability_libraries',
+        'composability/raydium_cpmm_program_composability_libraries'
+        ]
+    },
+    {
+      type: 'category',
+      label: 'Using Composability',
+      items: [
+        'composability/using_composability/setup_configure',
+        'composability/using_composability/transfer_sols',
+        'composability/using_composability/transfer_spl_token',
+        'composability/using_composability/swap_on_orca',
+        'composability/using_composability/swap_on_raydium',
+        'composability/using_composability/using_vrf',
+      ]
+    },
+    {
+      type: 'doc',
+      label: 'Solana Native SDK',
+      id: 'composability/sdk_solana_native'
+    },
+    {
+      type: 'doc',
+      label: 'Using SPL Tokens',
+      id: 'composability/using_spl_tokens'
+    },
+    { type: 'html', value: '<div class="sidebar-menu-divider"/>' },
+    { type: 'html', value: '<h4 class="sidebar-menu-title">User Guide</h4>' },    
+    {
+      type: 'category',
+      label: 'Neon EVM Overview',
       items: ['about/why_neon', 'about/neon_ecosystem', 'about/how_it_works', 'about/latest_updates']
     },
     {
       type: 'doc',
-      label: '🔑 Connect Wallet',
+      label: 'Connect Wallet',
       id: 'wallet/metamask_setup'
-    },
+    },    
     {
       type: 'category',
-      label: '💰 Tokens',
-      items: ['tokens/neon_token', 'tokens/gas_fees', 'tokens/token_list']
-    },
+      label: 'Transfer Tokens',
+     items: ['token_transferring/neonpass_usage', 'token_transferring/neon_transfer']
+    },    
     {
-      type: 'category',
-      label: '🛰 Transfer Tokens',
-      items: ['token_transferring/neonpass_usage', 'token_transferring/neon_transfer']
-    },
-    {
-      type: 'category',
-      label: '🎬 Demo',
-      items: ['about/Memecoin_Launchpad_Raydium_Integration', 'about/POC_Aave_V3_flash_loan_with_composability_requests_to_Solana']
-    },
+      type: 'doc',
+      label: 'Voting Process',
+      id: 'governance/proposals'
+    },    
     { type: 'doc', label: 'Neon Proxy API', id: 'api/neon-api' },
     {
       type: 'category',
-      label: '💬 FAQ',
+      label: 'FAQ',
       items: ['faq/neon-brief-faq', 'faq/neonpass']
     },
     { type: 'html', value: '<div class="sidebar-menu-divider"/>' },
-    { type: 'html', value: '<h4 class="sidebar-menu-title">Develop</h4>' },
+    { type: 'html', value: '<h4 class="sidebar-menu-title">Developer Guide</h4>' },
     {
       type: 'doc',
       label: 'Get Started',
@@ -111,6 +164,30 @@ const sidebars = {
     },
     {
       type: 'category',
+      label: 'Tools',
+      items: [
+        {
+         type: 'link',
+         label: 'Block Explorer',
+         href: 'https://neon.blockscout.com/'
+        },
+        {
+         type: 'link',
+         label: 'NeonPass',
+         href: 'https://devnet.neonpass.live/'
+        },
+        {
+         type: 'link',
+         label: 'NeonFaucet',
+         href: 'https://neonfaucet.org/'
+        },        
+      ]
+    },
+
+
+    
+    {
+      type: 'category',
       label: 'Configure Dev Tools',
       items: [
         'developing/deploy_facilities/configure_hardhat',
@@ -123,52 +200,7 @@ const sidebars = {
       label: 'Set up Local Neon EVM',
       id: 'developing/dev_environment/local_proxy_remote_solana'
     },
-    { type: 'html', value: '<div class="sidebar-menu-divider"/>' },
-    { type: 'html', value: '<h4 class="sidebar-menu-title">Interact with Solana</h4>' },
-    {
-      type: 'doc',
-      label: 'Common Solana Terminology',
-      id: 'composability/common_solana_terminology'
-    },
-    {
-      type: 'doc',
-      label: 'ICallSolana Interface',
-      id: 'composability/call_solana_interface'
-    },
-    {
-      type: 'category',
-      label: 'Composability Libraries',
-      items: [
-        'composability/composability_libraries',
-        'composability/system_program_composability_libraries',
-        'composability/spl_token_program_composability_libraries',
-        'composability/metaplex_program_composability_libraries',
-        'composability/associated_token_program_composability_libraries',
-        'composability/raydium_cpmm_program_composability_libraries'
-        ]
-    },
-    {
-      type: 'category',
-      label: 'Using Composability',
-      items: [
-        'composability/using_composability/setup_configure',
-        'composability/using_composability/transfer_sols',
-        'composability/using_composability/transfer_spl_token',
-        'composability/using_composability/swap_on_orca',
-        'composability/using_composability/swap_on_raydium',
-        'composability/using_composability/using_vrf',
-      ]
-    },
-    {
-      type: 'doc',
-      label: 'Solana Native SDK',
-      id: 'composability/sdk_solana_native'
-    },
-    {
-      type: 'doc',
-      label: 'Using SPL Tokens',
-      id: 'composability/using_spl_tokens'
-    },
+
     { type: 'html', value: '<div class="sidebar-menu-divider"/>' },
     { type: 'html', value: '<h4 class="sidebar-menu-title">Learn</h4>' },
     {
